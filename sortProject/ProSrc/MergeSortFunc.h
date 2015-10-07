04f760e6f8462ead6c0e758aa3821e1f51a7bc72
feiyu19900824@163.com <huangjinyu@localhost>
2015-10-07 14:32:40 -0400

    [who]By Huangjinyu [what]implement the merge sort func(not tested)

--- sortProject/ProSrc/MergeSortFunc.h
+++ sortProject/ProSrc/MergeSortFunc.h
@@ -11,6 +11,12 @@ class MergeSortFunc : public SortFuncAbstra
 		virtual ~MergeSortFunc();
 		virtual bool sortFunc(vector<int> &vecInput);
 		virtual void printSortFuncID();
+
+	private://func
+		bool mSort(vector<int> &vecInput);
+		void merge(vector<int> &vecInput , vector<int> &vecOutput , int nMid);
+		void mergeSort(vector<int> &vecInput , vector<int> &vecOutput
+						, int nLow , int nMid , int nHigh);
 };
 
 #endif

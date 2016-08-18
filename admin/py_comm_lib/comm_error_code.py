7d28f6ea3e08ed9e5a9658d30b4eb55fe0037844
JimmyHuang0824 <1352783476@qq.com>
2016-08-18 10:21:51 -0700

    加入全工程的编译脚本

---
+++ admin/py_comm_lib/comm_error_code.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+#
+#   Author  :   apachehuang
+#   Date    :   16/08/12 18:50:27
+#   Desc    :   公共错误码定义
+#
+
+INPUT_PARAM_TYPE_INVALID = 100001    #函数输入参数类型无效
+INPUT_PARAM_IS_NOT_EXIST = 100002    #输入参数指向的内容不存在
